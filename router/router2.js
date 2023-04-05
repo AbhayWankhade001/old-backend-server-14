d50cd3fa3f6d45fdb1fb8370f680a6506809f15d
AbhayWankhade001 <110882315+AbhayWankhade001@users.noreply.github.com>
2023-04-05 18:13:42 +0530

    Update router2.js

--- router/router2.js
+++ router/router2.js
@@ -1,5 +1,5 @@
 import express from "express";
-import User,{Employee , Admin} from "../model/User.model.js";
+import {Employee , Admin} from "../model/User.model.js";
 import nodemailer from "nodemailer"
 
 import config from "./config.js";
@@ -7,28 +7,85 @@ import jwt from 'jsonwebtoken';
 import { BankDetails } from "../model/User.model.js";
 
 import bcrypt from "bcrypt"
+import cryptoRandomString from "crypto-random-string";
+import AWS from "aws-sdk";
+import multer from 'multer';
 const router2 = express.Router();
 
 
 //>>>>>>>>>>>>>>>>>>>>>>>>>>POST API are here  <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<//
 
-router2.post("/addBankDetails", async (req, res) => {
+// router2.post("/addBankDetails", async (req, res) => {
+//   const token = req.headers.authorization?.split(' ')[1] || req.cookies.token;
+//   if (!token) {
+//     return res.status(401).send('Unauthorized');
+//   }
+
+//   try {
+//     const token = req.headers.authorization.split(' ')[1];
+//     const decodedToken = jwt.verify(token, config.JWT_SECRET,  { algorithm: 'HS256' });
+//     const employeeId = decodedToken.employeeId;
+//     const adminId = decodedToken.adminId;    
+//     console.log(decodedToken);
+//     console.log(employeeId);
+//     console.log(adminId);
+
+//     // Find the existing bank details object for the employee
+//     let bankDetails = await BankDetails.findOne({ employeeId: employeeId });
+//     console.log(employeeId)
+//     if (bankDetails) {
+//       // Update the existing bank details object with the new form data
+//       bankDetails.accountholdername = req.body.accountholdername;
+//       bankDetails.mobilenumber = req.body.mobilenumber;
+//       bankDetails.acctype = req.body.acctype;
+//       bankDetails.bankname = req.body.bankname;
+//       bankDetails.branchname = req.body.branchname;
+//       bankDetails.ifsc = req.body.ifsc;
+//       bankDetails.pannumber = req.body.pannumber;
+//       bankDetails.bankaccnumber = req.body.bankaccnumber;
+//     } else {
+//       // Create a new bank details object and populate it with form data
+//       bankDetails = new BankDetails({
+//         employeeId: employeeId,
+//         adminId: adminId,
+//         accountholdername: req.body.accountholdername,
+//         mobilenumber: req.body.mobilenumber,
+//         acctype: req.body.acctype,
+//         bankname: req.body.bankname,
+//         branchname: req.body.branchname,
+//         ifsc: req.body.ifsc,
+//         pannumber: req.body.pannumber,
+//         bankaccnumber:req.body.bankaccnumber,
+//       });
+//     }
+
+//     // Save the bank details object to the database
+//     const savedBankDetails = await bankDetails.save();
+//     console.log("Bank details added/updated successfully. Employee ID:", employeeId, "Admin ID:", adminId, "Form data:", savedBankDetails);
+//     res.send(savedBankDetails);
+//   } catch (err) {
+//     console.log("Error adding/updating bank details:", err.message);
+//     console.log('Error decoding JWT token:', err.message);
+//     return res.status(401).send('Unauthorized');
+//   }
+// });
+router2.post('/addBankDetails', async (req, res) => {
   const token = req.headers.authorization?.split(' ')[1] || req.cookies.token;
   if (!token) {
     return res.status(401).send('Unauthorized');
   }
 
   try {
-    const token = req.headers.authorization.split(' ')[1];
-    const decodedToken = jwt.verify(token, config.JWT_SECRET,  { algorithm: 'HS256' });
+    const decodedToken = jwt.verify(token, config.JWT_SECRET, { algorithm: 'HS256' });
     const employeeId = decodedToken.employeeId;
     const adminId = decodedToken.adminId;    
     console.log(decodedToken);
     console.log(employeeId);
-    console.log(employeeId.adminId);
+    console.log(adminId);
 
     // Find the existing bank details object for the employee
     let bankDetails = await BankDetails.findOne({ employeeId: employeeId });
+    console.log(employeeId)
     if (bankDetails) {
       // Update the existing bank details object with the new form data
       bankDetails.accountholdername = req.body.accountholdername;
@@ -65,12 +122,6 @@ router2.post("/addBankDetails", async (req, res) => {
     return res.status(401).send('Unauthorized');
   }
 });
-
-
-
-
-
-
 //>>>>>>>>>>>>>>>>>>>>>>>>>>GET API are here  <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<//
 
 
@@ -177,13 +228,12 @@ function checkRequiredFields(obj, fields) {
 
 router2.put('/createEmployees', async (req, res) => {
   try {
-    const authorizationHeader = req.headers.authorization;
+    const token = req.headers.authorization?.split(' ')[1] || req.cookies.token;
     const { newPassword, confirmNewPassword, email } = req.body;
     let employee;
 
-    if (authorizationHeader) {
+    if (token) {
       // if token is present
-      const token = authorizationHeader.split(' ')[1];
       const tokenData = jwt.verify(token, config.JWT_SECRET,  { algorithm: 'HS256' });
       const adminId = tokenData.adminId;
       const employeeId = tokenData.employeeId;
@@ -192,7 +242,7 @@ router2.put('/createEmployees', async (req, res) => {
       console.log('Admin ID:', adminId);
       console.log('Employee ID:', employeeId);
       console.log('Started resetting employee password');
-      console.log('authorizationHeader:', authorizationHeader);
+      console.log('authorizationHeader:', token);
       console.log('token:', token);
       console.log('tokenData:', tokenData);
       console.log('adminId:', adminId);
@@ -252,5 +302,203 @@ router2.put('/createEmployees', async (req, res) => {
     }
   });
   
+  
+  
+  
+  
+let otpInfo = { email: '', otp: '' };
+
+// Route to send OTP to user's email
+router2.post('/forgotPassword/sendOTP', async (req, res) => {
+  try {
+    const { email } = req.body;
+    const employee = await Employee.findOne({ email });
+    if (!employee) {
+      throw new Error('No account found with the provided email');
+    }
+
+    // Check if there is already an OTP in the employee's document
+    if (employee.otp) {
+      employee.otpExpiry = new Date(Date.now() - 1); // Set the OTP's expiry time to a time in the past
+      await employee.save();
+    }
+
+    // Generate OTP
+    const otp = cryptoRandomString({ length: 6 });
+    console.log(otp);
+
+    // Save OTP and its expiry time in employee's document
+    const otpExpiry = new Date(Date.now() + 10 * 60 * 1000); // OTP will expire after 10 minutes
+    employee.otp = otp;
+    employee.otpExpiry = otpExpiry;
+    await employee.save();
+
+    // Save email and OTP for verification
+    otpInfo.email = email;
+    otpInfo.otp = otp;
+
+    // Send OTP to employee's email using NodeMailer
+    const mailOptions = {
+      from: config.EMAIL_ADDRESS,
+      to: email,
+      subject: 'Reset your password for Cling Multi Solution',
+      html: `<p>Hello ${employee.firstName},</p>
+             <p>Your OTP for resetting your Cling Multi Solution account password is:</p>
+             <p style="font-size: 24px; font-weight: bold;">${otp}</p>
+             <p>Please use this OTP to reset your password within 10 minutes of receiving this email.</p>
+             <p>Thank you!</p>`,
+    };
+
+    transporter.sendMail(mailOptions, (error, info) => {
+      if (error) {
+        console.log('Error:', error.message);
+        throw new Error('Could not send OTP to the provided email');
+      } else {
+        console.log('Email sent:', info.response);
+        res.send({ message: 'OTP sent successfully' });
+      }
+    });
+  } catch (err) {
+    console.log('Error:', err.message);
+    res.status(400).send({ message: err.message });
+  }
+});
+
+// Route to verify OTP
+router2.post('/forgotPassword/verifyOTP', async (req, res) => {
+  try {
+    const { otp } = req.body;
+
+    // Verify OTP
+    if (otp !== otpInfo.otp || new Date() > otpInfo.otpExpiry) {
+      throw new Error('Invalid or expired OTP');
+    }
+
+    // Retrieve employee with the provided email
+    const employee = await Employee.findOne({ email: otpInfo.email });
+    if (!employee) {
+      throw new Error('No account found with the provided email');
+    }
+
+    // Save credentials in a variable
+    const credentials = { email: otpInfo.email, otp };
+
+    res.send({ message: 'OTP verification successful', credentials });
+  } catch (err) {
+    console.log('Error:', err.message);
+    res.status(400).send({ message: err.message });
+  }
+});
+
+
+
+
+router2.put('/forgotPassword/updatePassword', async (req, res) => {
+  try {
+    const { newPassword } = req.body; // add this line
+    const { email } = otpInfo;
+    const employee = await Employee.findOne({ email });
+    
+    if (!employee) {
+      throw new Error('No account found with the provided email');
+    }
+    
+    const saltRounds = 10;
+    const hashedPassword = await bcrypt.hash(newPassword, saltRounds);
+
+    employee.password = hashedPassword;
+    await employee.save();
+
+    // Remove OTP and its expiry time from employee's document
+    employee.otp = '';
+    employee.otpExpiry = '';
+    await employee.save();
+
+    // Clear resetCredentials
+    otpInfo = { email: '', otp: '' };
+
+    console.log('Password updated successfully'); // Debugging purposes only
+
+    res.send({ message: 'Password updated successfully' });
+  } catch (err) {
+    console.log('Error:', err.message);
+    res.status(400).send({ message: err.message });
+  }
+});
+
+
+
+
+
+
+// Set up AWS S3 configuration
+AWS.config.update({
+  bucketName: "clinginvoice",
+        region: "ap-southeast-2",
+        accessKeyId: 'AKIA3ACAYLO7SQIXYFPJ',
+        secretAccessKey: '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',
+});
+
+// Create an S3 instance
+const s3 = new AWS.S3();
+
+// Set up Multer middleware to handle file uploads
+const storage = multer.memoryStorage();
+const upload = multer({ storage });
+
+
+
+
+
+
+
+
+
+// Define a function to fetch a PDF file from S3 and send it as an email attachment
+async function sendPdfByEmail(pdfKey, recipientEmail, message) {
+  try {
+    // Fetch the PDF file from S3
+    const s3Params = {
+      Bucket: 'clinginvoice',
+      Key: pdfKey,
+    };
+    const pdfObject = await s3.getObject(s3Params).promise();
+
+    // Set up the email options
+    const mailOptions = {
+      from: 'abhaywankhade2004@gmil.com',
+      to: recipientEmail,
+      subject: 'PDF attachment',
+      text: message,
+      attachments: [
+        {
+          filename: pdfKey,
+          content: pdfObject.Body,
+        },
+      ],
+    };
+
+    // Send the email
+    const info = await transporter.sendMail(mailOptions);
+    console.log(`Email sent: ${info.messageId}`);
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+// Define the API endpoint
+router2.post('/send-pdf', upload.single('pdf'), async (req, res) => {
+  try {
+    const pdfKey = req.body.pdfName;
+    const recipientEmail = req.body.email;
+    const message = req.body.message;
+    await sendPdfByEmail(pdfKey, recipientEmail, message);
+    res.status(200).send('Email sent successfully');
+  } catch (error) {
+    console.error(error);
+    res.status(500).send('Error sending email');
+  }
+});
+
   
 export default router2;
